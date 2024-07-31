5522dbd2023db8c2ed264d89c29c9a5bf033ef35
richmondazadze <richmondazadze1313@gmail.com>
2024-07-31 05:27:13 +0000

    New Cat

--- components/modals/AddExpensesModal.js
+++ components/modals/AddExpensesModal.js
@@ -1,4 +1,4 @@
-import { useState, useContext } from "react";
+import { useState, useContext, useRef } from "react";
 import { financeContext } from "@/lib/store/finance-context";
 
 import { v4 as uuidv4 } from "uuid";
@@ -6,11 +6,14 @@ import Modal from "../modal";
 
 function AddExpensesModal({ show, onClose }) {
   const [expenseAmount, setExpenseAmount] = useState("");
+  const [showAddExpense, setShowAddExpense] = useState(false);
   const [selectedCategory, setSelectedCategory] = useState(null);
 
-  const { expenses } = useContext(financeContext);
+  const { expenses, addExpenseItem, addCategory } = useContext(financeContext);
 
-  const addExpenseItemHanlder = () => {
+  const titleRef = useRef();
+  const colorRef = useRef();
+  const addExpenseItemHanlder = async () => {
     const expense = expenses.find((e) => {
       return e.id === selectedCategory;
     });
@@ -28,11 +31,28 @@ function AddExpensesModal({ show, onClose }) {
       ],
     };
 
-    console.log(newExpense);
+    try {
+      await addExpenseItem(selectedCategory, newExpense);
 
-    setExpenseAmount("");
-    setSelectedCategory(null);
-    onClose();
+      console.log(newExpense);
+
+      setExpenseAmount("");
+      setSelectedCategory(null);
+      onClose();
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+  const addCategoryHandler = async () => {
+    const title = titleRef.current.value;
+    const color = colorRef.current.value;
+
+    try {
+      await addCategory({ title, color, total: 0 });
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
   return (
@@ -55,7 +75,46 @@ function AddExpensesModal({ show, onClose }) {
 
       {expenseAmount > 0 && (
         <div className="flex flex-col gap-4 mt-6">
-          <h4 className="text-xl capitalize">Select expense category</h4>
+          <div className="flex items-center justify-between">
+            <h5 className="text-l capitalize">Select expense category</h5>
+            <button
+              onClick={() => {
+                setShowAddExpense(true);
+              }}
+              className="text-lime-400"
+            >
+              + New Category
+            </button>
+          </div>
+
+          {showAddExpense && (
+            <div>
+              <input type="text" placeholder="Enter title" ref={titleRef} />
+              <div className="flex items-center justify-between my-3">
+                <div className="flex items-center gap-4 mx-2">
+                  <label>Pick Color</label>
+                  <input className="w-20 h-10" type="color" ref={colorRef} />
+                </div>
+                <div className="flex items-center gap-4">
+                  <button
+                    onClick={addCategoryHandler}
+                    className="btn btn-primary-outline"
+                  >
+                    Create
+                  </button>
+                  <button
+                    onClick={() => {
+                      setShowAddExpense(false);
+                    }}
+                    className="btn btn-danger"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </div>
+          )}
+
           {expenses.map((expense) => {
             return (
               <button
