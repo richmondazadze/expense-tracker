11e20e2e8668fe7a744350ad49c42bb5d9688d99
richmondazadze <richmondazadze1313@gmail.com>
2024-08-03 18:51:24 +0000

    probable fix redirection

--- components/SignIn.js
+++ components/SignIn.js
@@ -32,7 +32,7 @@ function SignIn() {
     const android = /Android/.test(navigator.userAgent);
 
     if (iOS) {
-      window.location = url; // Opens in Safari
+      alert("Please open this link in Safari: " + url);
     } else if (android) {
       const intentUrl = `intent://${url.replace(
         /^https?:\/\//,
