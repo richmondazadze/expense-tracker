4392f7b37a7ce3e0e859e610ff54cc85175de126
richmondazadze <richmondazadze1313@gmail.com>
2024-08-03 19:09:32 +0000

    New Solution

--- components/SignIn.js
+++ components/SignIn.js
@@ -32,7 +32,7 @@ function SignIn() {
     const android = /Android/.test(navigator.userAgent);
 
     if (iOS) {
-      window.location.href = `x-web-search://${url}`;
+      window.location.href = window.location.href = `applewebdata://open?url=${encodeURIComponent(url)}`;
     } else if (android) {
       const intentUrl = `intent://${url.replace(
         /^https?:\/\//,
