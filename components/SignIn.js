a028bf82a4f64b3ec6f1f474d9a1e15a82491551
richmondazadze <richmondazadze1313@gmail.com>
2024-08-03 19:19:10 +0000

    Force Open Fix

--- components/SignIn.js
+++ components/SignIn.js
@@ -32,7 +32,12 @@ function SignIn() {
     const android = /Android/.test(navigator.userAgent);
 
     if (iOS) {
-      window.location.href = window.location.href = `applewebdata://open?url=${encodeURIComponent(url)}`;
+      const a = document.createElement("a");
+      a.href = url;
+      a.rel = "noopener noreferrer";
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
     } else if (android) {
       const intentUrl = `intent://${url.replace(
         /^https?:\/\//,
